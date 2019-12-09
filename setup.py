1badd5362c3546545f2668f25488a2a1c14ae0f7
Kostas Stathoulopoulos <k.stathoylopoylos@gmail.com>
2019-12-09 12:10:34 +0000

    Setup Nesta Data Science cookiecutter

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='fnf',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A repository for analysis for the Future News Fund project.',
+    author='nestauk',
+    license='MIT',
+)
