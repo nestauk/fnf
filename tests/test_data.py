634becaec22926db42afe1930a13b0406e00fa25
Kostas Stathoulopoulos <k.stathoylopoylos@gmail.com>
2019-12-09 17:43:44 +0000

    update tests formatting

--- tests/test_data.py
+++ tests/test_data.py
@@ -6,13 +6,17 @@ from fnf.data.query_mag_composite import build_composite_expr
 
 
 def test_build_composite_queries_correctly():
-    assert build_composite_expr(["dog", "cat"], "F.FN", 2000) == "expr=OR(And(Composite(F.FN='dog'), Y>=2000), And(Composite(F.FN='cat'), Y>=2000))"
+    assert (
+        build_composite_expr(["dog", "cat"], "F.FN", 2000)
+        == "expr=OR(And(Composite(F.FN='dog'), Y>=2000), And(Composite(F.FN='cat'), Y>=2000))"
+    )
+
 
 @mock.patch("fnf.data.query_mag_composite.requests.post", autospec=True)
 def test_query_mag_api_sends_correct_request(mocked_requests):
     sub_key = 123
     fields = ["Id", "Ti"]
-    expr ="expr=OR(And(Composite(F.FN='dog'), Y>=2000), And(Composite(F.FN='cat'), Y>=2000))"
+    expr = "expr=OR(And(Composite(F.FN='dog'), Y>=2000), And(Composite(F.FN='cat'), Y>=2000))"
     query_mag_api(expr, fields, sub_key, query_count=10, offset=0)
     expected_call_args = mock.call(
         "https://api.labs.cognitive.microsoft.com/academic/v1.0/evaluate",
