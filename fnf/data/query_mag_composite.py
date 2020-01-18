3e32f745a9febd40d48af3b9f450b1f1541a0f4a
Kostas Stathoulopoulos <k.stathoylopoylos@gmail.com>
2020-01-18 10:35:00 +0000

    expand query composition

--- fnf/data/query_mag_composite.py
+++ fnf/data/query_mag_composite.py
@@ -1,29 +1,66 @@
 import requests
 from retrying import retry
+import logging
 
 ENDPOINT = "https://api.labs.cognitive.microsoft.com/academic/v1.0/evaluate"
 
 
-def build_composite_expr(query_values, entity_name, year):
-    """Builds a composite expression with ANDs in OR to be used as MAG query.
+# def build_composite_expr(query_values, entity_name, year):
+#     """Builds a composite expression with ANDs in OR to be used as MAG query.
+
+#     Args:
+#         query_values (:obj:`list` of str): Phrases to query MAG with. 
+#         entity_name (str): MAG attribute that will be used in query.
+#         year (int): We collect data in the [year, now] timeframe.
+#     Returns:
+#         (str) MAG expression.
+    
+#     """
+#     query_prefix_format = "expr=OR({})"
+#     and_queries = [
+#         "".join([f"And(Composite({entity_name}='{query_value}'), Y>={year})"])
+#         for query_value in query_values
+#     ]
+#     return query_prefix_format.format(", ".join(and_queries))
+
+
+def build_composite_expr(query_items, entity_name, year, max_length=16000):
+    """Builds and yields a composite expression with ANDs in OR to be used 
+    as MAG query from a list of items. Strings and integer items are formatted 
+    quoted and unquoted respectively, as per the MAG query specification.
+    The maximum accepted query length for the api appears to be around 16,000 characters.
+    The same module (without the composite) is in the nesta repo. 
 
     Args:
-        query_values (:obj:`list` of str): Phrases to query MAG with. 
-        entity_name (str): MAG attribute that will be used in query.
+        query_items (list): all items to be queried
+        entity_name (str): the mag entity to be queried ie 'Ti' or 'Id'
+        max_length (int): length of the expression which should not be exceeded. Yields
+            occur at or below this expression length
         year (int): We collect data in the [year, now] timeframe.
+
     Returns:
-        (str) MAG expression.
-    
+        (str): expression in the format expr=OR(entity_name=item1, entity_name=item2...)
+
     """
+    expr = []
+    length = 0
     query_prefix_format = "expr=OR({})"
-    and_queries = [
-        "".join([f"And(Composite({entity_name}='{query_value}'), Y>={year})"])
-        for query_value in query_values
-    ]
-    return query_prefix_format.format(", ".join(and_queries))
+    for item in query_items:
+        if type(item) == str:
+            formatted_item = f"And(Composite({entity_name}='{item}'), Y>={year})"
+        elif type(item) == int:
+            formatted_item = f"And(Composite({entity_name}={item}), Y >={year})"
+        length = sum(len(e) + 1 for e in expr) + len(formatted_item) + len(query_prefix_format)
+        if length >= max_length:
+            yield query_prefix_format.format(','.join(expr))
+            expr.clear()
+        expr.append(formatted_item)
 
+    # pick up any remainder below max_length
+    if len(expr) > 0:
+        yield query_prefix_format.format(','.join(expr))
 
-@retry(stop_max_attempt_number=5)
+@retry(stop_max_attempt_number=1)
 def query_mag_api(expr, fields, subscription_key, query_count=1000, offset=0):
     """Posts a query to the Microsoft Academic Graph Evaluate API.
 
